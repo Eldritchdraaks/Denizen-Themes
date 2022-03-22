f2a3378322d2ae5bec104415cd6cc921b2bf6cec
Eldritchdraaks <101802705+Eldritchdraaks@users.noreply.github.com>
2022-03-22 16:39:03 -0500

    Add files via upload

--- Denizenthemes/mod.js
+++ Denizenthemes/mod.js
@@ -16,10 +16,10 @@ const cetus = `
         url(assets://archive/collection/Cetus/cetus_homebg_center.png) center center no-repeat fixed;
 }`
 
-const hepheastus = `
+const hephaestus = `
 #app > *, .tabFrame > div {
     --system-background:   
-        url(assets://archive/collection/Hepheastus/hepheastus_homebg_center.png) center center no-repeat fixed,
+        url(assets://archive/collection/Hephaestus/hephaestus_homebg_center.png) center center no-repeat fixed,
 	linear-gradient(180deg, #666666 0%, #555555 49%, #4b4b4b 50%, #595959 100%) fixed;
 }`
 
@@ -30,12 +30,12 @@ const echidna = `
 }`
 
 module.exports = {
-    title: "Denzien Browser Themes", 
+    title: "Denizen Browser Themes", 
     summary: "Themes based on the beta kids internet browsers!",
     author: "Eldritchdraaks",
-    modVersion: 1.0,
+    modVersion: 1.1,
     locked: "010030",
-    description: "Typheus, Cetus, Hepheastus, Echidna. In the early acts of homestuck we see internet browsers named after these Denizens. Why not style your Homestuck browsing experience with a theme just like them?<br><b>Previews:</b> <a href=\"assets://archive/collection/Typheus/typheus_preview.png\">Typheus</a> • <a href=\"assets://archive/collection/Cetus/cetus_preview.png\">Cetus</a> • <a href=\"assets://archive/collection/Hepheastus/hepheastus_preview.png\">Hepheastus</a> • <a href=\"assets://archive/collection/Echidna/echidna_preview.png\">Echidna</a>",
+    description: "Typheus, Cetus, Hephaestus, Echidna. In the early acts of homestuck we see internet browsers named after these Denizens. Why not style your Homestuck browsing experience with a theme just like them?<br><b>Previews:</b> <a href=\"assets://archive/collection/Typheus/typheus_preview.png\">Typheus</a> • <a href=\"assets://archive/collection/Cetus/cetus_preview.png\">Cetus</a> • <a href=\"assets://archive/collection/Hepheastus/hepheastus_preview.png\">Hepheastus</a> • <a href=\"assets://archive/collection/Echidna/echidna_preview.png\">Echidna</a>",
     styles: [],
 
     computed(api){
@@ -59,9 +59,9 @@ module.exports = {
                     {body: cetus}
                 )
                 break;
-            case 'hepheastus':
+            case 'hephaestus':
             computed.styles.push(
-                {body: hepheastus}
+                {body: hephaestus}
             	)
                 break;
             case 'echidna':
@@ -82,8 +82,8 @@ module.exports = {
             label: "Cetus",
             source: "./Cetus/cetus_theme.scss"
         },{
-            label: "Hepheastus",
-            source: "./Hepheastus/hepheastus_theme.scss"
+            label: "Hephaestus",
+            source: "./Hephaestus/hephaestus_theme.scss"
         },{
             label: "Echidna",
             source: "./Echidna/echidna_theme.scss"
@@ -109,8 +109,8 @@ module.exports = {
                 value: "cetus",
                 label: "Cetus"
             },{
-                value: "hepheastus",
-                label: "Hepheastus"
+                value: "hephaestus",
+                label: "Hephaestus"
             },{
                 value: "echidna",
                 label: "Echidna"
